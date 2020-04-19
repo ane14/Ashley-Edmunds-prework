38ed2df469be80e5dd506a65ad607a7bd7d2fa1a
ane14 <anedmunds97@gmail.com>
2020-04-18 21:22:06 -0400

    module 2 complete

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -1,51 +1,94 @@
-const college = ['badger', 'gator', 'tiger', 'hokie', 'aggie', 'deacon', 'wildcat', 'husky', 'lion', 'cougar', 'bruin'];
+const college = ['badger', 'gator', 'tiger', 'hokie', 'deacon', 'wildcat', 'husky', 'lion', 'cougar', 'bruin'];
+
 // Choose word randomly
 let randNum = Math.floor(Math.random() * college.length);
 let chosenWord = college[randNum];
-let rightWord = [];
-let wrongWord = [];
-let underScore = [];
-let remainingGuess = 0;
-let wins = 0;
+
+// HTML Classnames
+let rightWord      = [];
+let wrongWord      = [];
+let underScore     = [];
+let remainingGuess = 10;
+let wins           = 0;
+
 // Dom manipulation
 let docremainingGuess = document.getElementsByClassName('remainingGuess');
-let docUnderScore = document.getElementsByClassName('currentGuesses');
-let docRightGuess = document.getElementsByClassName('correctGuesses');
-let docWrongGuess = document.getElementsByClassName('alreadyGuessed');
-let docWin = document.getElementsByClassName('win');
+let docUnderScore     = document.getElementsByClassName('currentGuesses');
+let docRightGuess     = document.getElementsByClassName('correctGuesses');
+let docWrongGuess     = document.getElementsByClassName('alreadyGuessed');
+let docWin            = document.getElementsByClassName('win');
 
-// Create underscored based on length of word
-let generateUnderscore = () => {
-    for(let i = 0; i < chosenWord.length; i++) {
+// Create underscores based on length of word
+function generateUnderscore() {
+    for (let i = 0; i < chosenWord.length; i++) {
         underScore.push('_');
         docUnderScore[0].innerHTML = underScore.join(' ');
     }
     return underScore;
 }
+
 // Get user's guess
 document.addEventListener('keypress', (event) => {
-    let keyword = String.fromCharCode(event.keyCode); 
-// if users guess is right
+    let keyword = String.fromCharCode(event.keyCode);
+
+    // If user's guess is right
     if (chosenWord.indexOf(keyword) > -1) {
-    // add to right words array
-    rightWord.push(keyword);
-// replace underscore with right letter
-    underScore[chosenWord.indexOf(keyword)] = keyword;
-    docUnderScore[0].innerHTML = underScore.join(' ');
-    docRightGuess[0].innerHTML = rightWord;
 
-    if(underScore.join('') == chosenWord) {
-        alert('You Win'); 
-        
-    }
+        // Add to right words array
+        rightWord.push(keyword);
 
-}   
-else {
-    wrongWord.push(keyword);
-    docWrongGuess[0].innerHTML = wrongWord;
+        // Replace underscore with right letter
+        underScore[chosenWord.indexOf(keyword)] = keyword;
+        docUnderScore[0].innerHTML = underScore.join(' ');
+        docRightGuess[0].innerHTML = rightWord;
+
+        if (underScore.join('') == chosenWord) {
+            alert('You Win');
+
+            wins += 1;
+            docWin[0].innerHTML = wins;
+
+            resetGame();
+        }
     }
-    
+    else {
+        wrongWord.push(keyword);
+        docWrongGuess[0].innerHTML = wrongWord;
+
+        remainingGuess -= 1;
+        docremainingGuess[0].innerHTML = remainingGuess;
+
+        // Check to see if there are no more guesses
+        if (remainingGuess <= 0) {
+            alert('You Lost');
+
+            resetGame();
+        }
+    }
 });
 
+// Display underscores at the beginning of the game
+generateUnderscore().join(' ');
 
-docUnderScore.innerHTML = generateUnderscore().join(' ');
+function resetGame() {
+    // Reset game information
+    rightWord = [];
+    docRightGuess[0].innerHTML = rightWord;
+
+    wrongWord = [];
+    docWrongGuess[0].innerHTML = wrongWord;
+
+    underScore = [];
+    docUnderScore[0].innerHTML = underScore;
+
+    // Choose new word
+    randNum = Math.floor(Math.random() * college.length);
+    chosenWord = college[randNum];
+
+    // Reset underscores
+    generateUnderscore();
+
+    // Reset number of guesses
+    remainingGuess = 10;
+    docremainingGuess[0].innerHTML = remainingGuess;
+}
