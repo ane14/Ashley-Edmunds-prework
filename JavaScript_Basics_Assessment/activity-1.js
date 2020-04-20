a25a7f2514c052cf276c6d5ec34d2cce495ae3c1
ane14 <anedmunds97@gmail.com>
2020-04-19 22:22:58 -0400

    completed modules

--- JavaScript_Basics_Assessment/activity-1.js
+++ JavaScript_Basics_Assessment/activity-1.js
@@ -1,12 +1,21 @@
-9 * 9;
-// product should be 81
-56 / 12;
-// quotient should be 4.6667
-281 % 9;
-// remainder should be 2
-18 / 0;
-// answer should be infinity
-sum = '56' + '92';
-// answer comes to 5692
-42 + 52 === 100
-// false
+let product = 9 * 9;
+print(product);
+
+let quotient = 56 / 12;
+print(quotient)
+
+let remainder = 281 % 9;
+print(remainder)
+
+let variable = 18 / 0;
+print(variable)
+
+let sum = '56' + '92';
+print(sum)
+
+if (42 + 52 == 100) {
+    print(false); 
+}
+else {
+    print(true);
+}
